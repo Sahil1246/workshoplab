48d707b44cd868fc4355ce1877ff591717acbcb0
Sahil1246 <125081449+Sahil1246@users.noreply.github.com>
2024-11-13 20:35:57 +0530

    nothing

---
+++ lect-1/index.js
@@ -0,0 +1,22 @@
+let score=80
+let a=75
+if(score>75 && a>75){
+    console.log('A');
+    
+}
+else if(score>65 && a>65){
+    console.log('B');
+    
+}
+else if(score>55 && a>55){
+    console.log('C');
+    
+}
+else if(score>45 && a>45){
+    console.log('D');
+    
+}
+else if(score>35 && a>35){
+    console.log('fail');
+    
+}
---
+++ lect-2/index.js
@@ -0,0 +1,84 @@
+// variable hoisting
+
+// let a
+// console.log(a);
+// a=5
+
+
+// console.log(a);
+// let a=5
+
+// function hoisting
+// Sum()
+// function Sum(){
+//     console.log('hello');
+    
+// }
+
+// add()
+// var add=function(){
+//     console.log('hii');
+    
+// }
+// add()
+
+// // fun1()
+// var fun1=()=>{
+//     console.log('byee');
+    
+// }
+// fun1()
+// function outer(){
+//     let user ='hello';
+//     console.log('outer');
+//      function inner(){
+//         console.log(user);
+        
+//      }    
+//      return inner
+// }
+// let inner = outer()
+// inner ()
+
+
+// map return new arry
+// let arr=[1,2,3,4,5]
+
+
+//  let filterrData= arr.filter((a,b,c)=>{
+//     return a>2
+// })
+// console.log(filterrData);
+// let newArr= arr.map((a,b,c)=>{
+//     return a+5
+// }
+// )
+
+
+// arr.map((a,b,c)=>{          
+//     console.log(a);
+    
+// })
+// for(let i in arr){
+//     console.log(i);
+    
+// }
+// for(let i of arr){
+//     console.log(i);
+    
+// }
+// a Value
+// b index numbwr
+// c Array
+// arr.forEach((a,b,c)=>{
+//     console.log(c);
+    
+// })
+
+let arr=[1,2,3,45,6]
+    let data=arr,find(a)=>{
+        return a=5
+    }
+    console.log(data);
+    
+
